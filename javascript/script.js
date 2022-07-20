9aca2341a9b6fa209f9b2879ffb069f878bd17ed
mamo2003 <mlarraniaga1978@gmail.com>
2022-07-20 20:12:33 -0300

    prueba js

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+function pnombre(){
+    let nombre = prompt("bienvenido!! Ingresa tu nombre por favor:");
+    return nombre
+}
+
+window.onload(pnombre);
+console.log(pnombre);
+/* let nombre = queryselector(nombre).value;
+let visitante = queryselector("#visitante").innerhtml;
+
+visitante.innertext = Bienvenido + nombre;
+
+ */
