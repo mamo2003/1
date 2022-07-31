ab4aea2525f4157a6156c22f5759ec3a7f45c579
mamo2003 <mlarraniaga1978@gmail.com>
2022-07-31 19:44:12 -0300

    toma js!! listo, falta solo onclick al tocar <p>

--- javascript/main.js
+++ javascript/main.js
@@ -1,37 +1,29 @@
-// identificacion de usuario
-
-function visita(){let usuario = prompt("identificate");
-if(usuario === "")
-{alert("debes escribir tu nombre, no puede estar vacio");
-}return usuario
-}
-visita()
-
-usuario=visita();
-
 let visitante = document.querySelector("#visitante");
 
-let txtusuario=localStorage.setItem(usuario,usuario);
+function visita(){let usuario = prompt("identificate");
+while(usuario.length == 0)
+{alert("debes escribir tu nombre, no puede estar vacio");
+usuario = prompt("identificate");
+}return usuario
+}
+usuario=visita();
+let txtusuario=localStorage.setItem("usuario",usuario);
 txtusuario = usuario;
- let usuariorecupero= localStorage.getItem(usuario)
-
-/* usuario.addEventListener("change",() =>{txtusuario=localStorage.setItem("usuario",usuario)}); */  
-
-
+let usuariorecupero= localStorage.getItem("usuario");
+// onclick y remove posterior al poseer un nombre de usuario
+function click(){
+if (usuario.lenght == 0); {
+visitante.onclick = () => visita() && contado() ;
+}visitante.removeEventListener(onclick);    
+}
 // contador de clicks
 let numrecupero=parseInt(localStorage.getItem("contado"));
-let conteo = 0
+let conteo = 0 ;
 function contador() { let contado = conteo + 1;
+if(numrecupero > 0){ contado= numrecupero + 1}
 return contado;
 }
-contador()
-
 let contado=contador();
-
-console.log(numrecupero)
-console.log(contador())
 let numcontador= localStorage.setItem("contado", (contado));
 
-
-visitante.innerHTML=`<p><strong>Bienvenida/o  ${usuariorecupero}, Eres el visitante n°  ${contado} de nuestra pagina</strong></p>`;
-visitante.onclick = () => visita() & contador();
+visitante.innerHTML=`<p><strong> Bienvenida/o  ${usuariorecupero} , Eres el visitante n°  ${contado}  de nuestra pagina </strong></p>`;
