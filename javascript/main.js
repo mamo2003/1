af5bccee4e41211af71779c5ea5af04635c3c8ee
mamo2003 <mlarraniaga1978@gmail.com>
2022-08-13 09:50:45 -0300

    ok

--- javascript/main.js
+++ javascript/main.js
@@ -2,22 +2,31 @@ let visitante = document.querySelector("#visitante");
 
 let conteo=0;
 let contadoa=1;
-let contadob;
+let contadob=1;
 let numcontador=contadob;
+
+
 let numrecupero=parseInt(localStorage.getItem("contado"));
+console.log(numrecupero);
+
 function contador() { contadoa = conteo + contadoa;
 }if(numrecupero > 0){ contadob = numrecupero + contadoa;
+    numcontador=localStorage.setItem("contado",contadob);
+    console.log("ok",contadob)
 }
+
 numcontador=localStorage.setItem("contado",contadob);
 
 let usuario="";
-function visita () { let usuario = prompt("ingrese su nombre:");
+
+function visita () {usuario = prompt("ingrese su nombre:");
 if(usuario.length == 0) {alert("no ingreso un nombre , intentelo nuevamente")
 usuario= prompt("ingrese su nombre");
 let guardado=usuario;
-guardado=localStorage.setItem("usuario",usuario);
+guardado=localStorage.setItem("usuario",guardado);
 }else{visitante.innerHTML=`<p><strong> Bienvenida/o  ${usuario} , Eres el visitante n° ${contadob} de nuestra pagina </strong></p>`;
-    visitante.removeEventListener("click",visita ,contador)} 
+    visitante.removeEventListener("click",visita)
+} 
 }
 
-visitante.addEventListener("click",visita ,contador);
+visitante.addEventListener("click",visita);
